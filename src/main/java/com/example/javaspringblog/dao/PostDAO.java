a9e2f74b8603559a8b4530a61a8e5fc4db749500
sheluha <sheluha100@gmail.com>
2023-02-22 22:27:55 +0300

    from data jdbc to data jpa

--- src/main/java/com/example/javaspringblog/dao/PostDAO.java
+++ src/main/java/com/example/javaspringblog/dao/PostDAO.java
@@ -4,7 +4,6 @@ import com.example.javaspringblog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostDAO extends JpaRepository<Post,Integer> {
 
     //@Query("select * from Post order by created_at desc")
